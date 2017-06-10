b7015bce8b668c44c955ca9aa76ddaaa1ff9c065
Pierre Allard <pierallard@gmail.com>
2017-06-10 14:49:44 +0200

    Move create

--- src/game/Inventory.ts
+++ src/game/Inventory.ts
@@ -2,6 +2,18 @@
 import Play from "./state/Play";
 import {SimpleGame} from "../app";
 import {InventoryObject} from "./inventory_objects/InventoryObject";
+import {Steak} from "./inventory_objects/Steak";
+import {Lexomil} from "./inventory_objects/Lexomil";
+import {Battery} from "./inventory_objects/Battery";
+import {Lamp} from "./inventory_objects/Lamp";
+import {BouteilleAlcool} from "./inventory_objects/BouteilleAlcool";
+import {ZippoSec} from "./inventory_objects/ZippoSec";
+import {Tabac} from "./inventory_objects/Tabac";
+import {Cannabis} from "./inventory_objects/Cannabis";
+import {TabacBeuh} from "./inventory_objects/TabacBeuh";
+import {Feuilles} from "./inventory_objects/Feuilles";
+import {Rallonge} from "./inventory_objects/Rallonge";
+import {Couteau} from "./inventory_objects/Couteau";
 
 const INVENTORY_SIZE: number = (16+8)*4;
 const COLUMNS: number = 4;
@@ -49,6 +61,36 @@ export class Inventory {
 
         this.play.add.existing(top);
         this.play.add.existing(bottom);
+
+        this.createObjects();
+    }
+
+    createObjects() {
+        this.addObject(new InventoryObject(this.play, 'icesteak', 'le steak surgele', 'Un steak surgele'));
+        this.addObject(new Steak(this.play));
+        this.addObject(new Lexomil(this.play));
+        this.addObject(new Battery(this.play));
+        this.addObject(new Lamp(this.play));
+        this.addObject(new BouteilleAlcool(this.play));
+        this.addObject(new ZippoSec(this.play));
+        this.addObject(new Tabac(this.play));
+        this.addObject(new Cannabis(this.play));
+        this.addObject(new TabacBeuh(this.play));
+        this.addObject(new Feuilles(this.play));
+        this.addObject(new Rallonge(this.play));
+        this.addObject(new Couteau(this.play));
+
+        this.addObject(new InventoryObject(this.play, 'bedo', "le bedo", "Y'a plus qu'a allumer!"));
+        this.addObject(new InventoryObject(this.play, 'steaklexomil', 'le steaknifere', 'Voila qui pourrait endormir un cheval'));
+        this.addObject(new InventoryObject(this.play, 'engrais', "de l'engrais", '"Garder a portee des enfants". Perdu!'));
+        this.addObject(new InventoryObject(this.play, 'gode', 'le masseur de visage', "C'est le jouet de papa, ca vibre"));
+        this.addObject(new InventoryObject(this.play, 'escabeauInventory', "l'escabeau", "Je suis le roi du monde!"));
+        this.addObject(new InventoryObject(this.play, 'perceuse', 'la perceuse', "Des p'tits trous, des p'tits trous!"));
+        this.addObject(new InventoryObject(this.play, 'sachet', 'le sachet', "On dirait des graines!"));
+        this.addObject(new InventoryObject(this.play, 'lampePiles', 'la lampe UV', "J'ai perdu mes deux yeux"));
+        this.addObject(new InventoryObject(this.play, 'dvdporno', 'le DVD', '"Hairy mature gays"'));
+        this.addObject(new InventoryObject(this.play, 'zippo', 'le zippo', "Aie!"));
+        this.addObject(new InventoryObject(this.play, 'rallongecoupee', 'les fils electriques', "Y'a plus qu'a brancher!"));
     }
 
     pageUp() {
--- src/game/state/Play.ts
+++ src/game/state/Play.ts
@@ -4,23 +4,10 @@ import {Inventory} from "../Inventory";
 import {VerbRepository} from "../verbs/VerbRepository";
 import {MoveAction} from "../actions/MoveAction";
 import {Verb} from "../verbs/Verb";
-import {InventoryObject} from "../inventory_objects/InventoryObject";
-import {Steak} from "../inventory_objects/Steak";
-import {Lexomil} from "../inventory_objects/Lexomil";
 import {Scene} from "../groups/Scene";
 import {Sentence} from "../Sentence";
 import {GarageDoor} from "../scene_objects/GarageDoor";
 import {BedroomDoor} from "../scene_objects/BedroomDoor";
-import {Battery} from "../inventory_objects/Battery";
-import {Lamp} from "../inventory_objects/Lamp";
-import {BouteilleAlcool} from "../inventory_objects/BouteilleAlcool";
-import {ZippoSec} from "../inventory_objects/ZippoSec";
-import {Tabac} from "../inventory_objects/Tabac";
-import {Cannabis} from "../inventory_objects/Cannabis";
-import {TabacBeuh} from "../inventory_objects/TabacBeuh";
-import {Feuilles} from "../inventory_objects/Feuilles";
-import {Rallonge} from "../inventory_objects/Rallonge";
-import {Couteau} from "../inventory_objects/Couteau";
 import {ActionManager} from "../actions/ActionManager";
 import {Cursor} from "../Cursor";
 import {SimpleGame} from "../../app";
@@ -42,6 +29,7 @@ export default class Play extends Phaser.State
 
         this.inventory = new Inventory(this);
         this.actionManager = new ActionManager(this);
+        this.verbRepository = new VerbRepository(this);
         this.debug = false;
     }
 
@@ -49,26 +37,15 @@ export default class Play extends Phaser.State
     {
         this.scene = new Scene(this);
         this.game.add.existing(this.scene);
-
         this.inventory.create();
-
         this.sentence = new Sentence(this.game);
-
-        this.verbRepository = new VerbRepository(this);
-        this.verbRepository.render();
-
+        this.verbRepository.create();
         this.scene.createBackground();
-
         this.addBackground();
-
         this.scene.createObjects();
-        this.createInventoryObjects();
-
         this.baby = new Baby(this);
         this.scene.addMultiple(this.baby.getSprites());
-
         this.scene.createObjectSecond();
-
         this.cursor = new Cursor(this);
 
         if (this.debug) {
@@ -81,7 +58,7 @@ export default class Play extends Phaser.State
     {
         this.actionManager.execute();
         this.cursor.update();
-
+        this.verbRepository.update();
     }
 
     getBaby(): Baby {
@@ -107,34 +84,6 @@ export default class Play extends Phaser.State
         sprite.scale.setTo(SimpleGame.SCALE);
         sprite.inputEnabled = true;
         sprite.events.onInputDown.add(this.move, this);
-    }
-
-    private createInventoryObjects() {
-        this.inventory.addObject(new InventoryObject(this, 'icesteak', 'le steak surgele', 'Un steak surgele'));
-        this.inventory.addObject(new Steak(this));
-        this.inventory.addObject(new Lexomil(this));
-        this.inventory.addObject(new Battery(this));
-        this.inventory.addObject(new Lamp(this));
-        this.inventory.addObject(new BouteilleAlcool(this));
-        this.inventory.addObject(new ZippoSec(this));
-        this.inventory.addObject(new Tabac(this));
-        this.inventory.addObject(new Cannabis(this));
-        this.inventory.addObject(new TabacBeuh(this));
-        this.inventory.addObject(new Feuilles(this));
-        this.inventory.addObject(new Rallonge(this));
-        this.inventory.addObject(new Couteau(this));
-
-        this.inventory.addObject(new InventoryObject(this, 'bedo', "le bedo", "Y'a plus qu'a allumer!"));
-        this.inventory.addObject(new InventoryObject(this, 'steaklexomil', 'le steaknifere', 'Voila qui pourrait endormir un cheval'));
-        this.inventory.addObject(new InventoryObject(this, 'engrais', "de l'engrais", '"Garder a portee des enfants". Perdu!'));
-        this.inventory.addObject(new InventoryObject(this, 'gode', 'le masseur de visage', "C'est le jouet de papa, ca vibre"));
-        this.inventory.addObject(new InventoryObject(this, 'escabeauInventory', "l'escabeau", "Je suis le roi du monde!"));
-        this.inventory.addObject(new InventoryObject(this, 'perceuse', 'la perceuse', "Des p'tits trous, des p'tits trous!"));
-        this.inventory.addObject(new InventoryObject(this, 'sachet', 'le sachet', "On dirait des graines!"));
-        this.inventory.addObject(new InventoryObject(this, 'lampePiles', 'la lampe UV', "J'ai perdu mes deux yeux"));
-        this.inventory.addObject(new InventoryObject(this, 'dvdporno', 'le DVD', '"Hairy mature gays"'));
-        this.inventory.addObject(new InventoryObject(this, 'zippo', 'le zippo', "Aie!"));
-        this.inventory.addObject(new InventoryObject(this, 'rallongecoupee', 'les fils electriques', "Y'a plus qu'a brancher!"));
     }
 
     public render() {
